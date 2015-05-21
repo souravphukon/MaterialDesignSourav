dcd5603549530a9d2e258356f7298c9394371f94
souravphukon <sourov.borphukon@gmail.com>
2015-05-21 23:34:22 +0530

    Material Design

---
+++ app/src/main/java/com/example/sourav/materialdesigns/NavigationDrawerFragment.java
@@ -0,0 +1,123 @@
+package com.example.sourav.materialdesigns;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    public static final String PREF_FILE_NAME = "testpref";
+    public static final String KEY_USER_LEARN_DRAWER = "user_learn_drawer";
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private boolean mUserLearnUser;
+    private boolean mFromSaveInstanceState;
+    private View containerView;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        mUserLearnUser = Boolean.valueOf(readFromPreferences(getActivity(), KEY_USER_LEARN_DRAWER, "false"));
+        if (savedInstanceState != null)//Coming back from rotation
+        {
+            mFromSaveInstanceState = true;
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setup(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        containerView = getActivity().findViewById(fragmentId);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                if (!mUserLearnUser) {
+                    mUserLearnUser = true;
+
+                    saveToPreferences(getActivity(), KEY_USER_LEARN_DRAWER, mUserLearnUser + "");
+
+                }
+                getActivity().invalidateOptionsMenu();
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+
+                if (slideOffset < 0.6) {
+                    toolbar.setAlpha(1 - slideOffset);
+                }
+            }
+        };
+
+        if (!mUserLearnUser && !mFromSaveInstanceState) {
+            mDrawerLayout.openDrawer(containerView);
+
+        }
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                actionBarDrawerToggle.syncState();
+            }
+        });
+
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+      /* SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString(preferenceName,preferenceValue);
+        editor.apply();*/
+        return sharedPreferences.getString(preferenceName, defaultValue);
+
+    }
+}
+
